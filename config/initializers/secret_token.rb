2eb130ceceff1fafc27e73d6032a8109885f5430
Mark Wetzel <mark@markwetzel.com>
2013-10-10 23:44:42 -0400

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Day3::Application.config.secret_key_base = 'daaa231283d82ebe27952f14745b1b8ee7d5bd96f9630e9668a3cb2ddb77b880a7766a71bfbbc36f3fb152b9d4d375b4ebbc18a181545ce9479928786157d927'
